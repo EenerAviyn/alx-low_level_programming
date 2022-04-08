bfdd0bcb9b4726fdf541ee1916b3131b41f0c746
EenerAviyn <mulelarenee18@gmail.com>
2022-04-08 14:59:54 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,22 +5,28 @@
  * Description: prints the alphabet in lowercase, followed by a new line.
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 int n;
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+
 if (n > 0)
 {
 printf("%d is positive\n", n);
 }
+
 if (n < 0)
 {
 printf("%d is negative\n", n);
 }
+
 if (n == 0)
 {
 printf("%d is zero\n", n);
 }
+
 return (0);
 }
