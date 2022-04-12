3332fc296316f5d8002c81ad36282eb474bd122b
EenerAviyn <mulelarenee18@gmail.com>
2022-04-12 10:31:13 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print alphabet in lowercase followed by new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+void print_aphabet(void)
+{
+	printf("putchar(a..z)");
+	return (0);
+}
