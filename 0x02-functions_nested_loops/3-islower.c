2e295426e29b0f6e73c78f15fe5088b363fd9925
EenerAviyn <mulelarenee18@gmail.com>
2022-04-12 23:23:51 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,8 +3,11 @@
  * int _islower(int c) - Entry point
  *
  * Description: check for lower case letters
+ *
  * _islower - check for lower case letters
+ *
  * @c: character to check letter case
+ *
  * Return: 1 (if lowercase) or 0 (otherwise)
  */
 int _islower(int c)
