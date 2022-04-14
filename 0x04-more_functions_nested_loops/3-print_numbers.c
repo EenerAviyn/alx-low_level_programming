6d3e32119c150f7b417a778f36de3a0d01a410d1
EenerAviyn <mulelarenee18@gmail.com>
2022-04-14 04:23:58 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - print 0-9 then new line
+ * @n: number to print
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int n;
+
+	while (n = 0; n < 10; n++)
+	{
+		_putchar("%n", n);
+		_putchar("\n");
+	}
+}
