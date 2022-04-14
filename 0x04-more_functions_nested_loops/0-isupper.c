67721c37075d3a72b619be8959031f42d33a6f5a
EenerAviyn <mulelarenee18@gmail.com>
2022-04-14 04:04:58 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - Entry point
+ * Description: function to check for uppercase character
+ * @c: check case
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+	char c = 'c';
+
+	if (c == 99)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
