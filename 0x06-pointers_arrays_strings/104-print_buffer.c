344cd2167bd92459f74c1f21279ed705a87dec30
Renée Mulela <42552984+EenerAviyn@users.noreply.github.com>
2022-04-23 22:57:22 +0300

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -20,10 +20,24 @@ void print_buffer(char *b, int size)
 			printf("%.8x:", j);
 			for(n = m; n < m + 10; n++)
 			{
-				if (k % 2 == 0)
+				if (n % 2 == 0)
 					printf(" ");
-				if (k < size)
+				if (n < size)
 					printf("%.2x", *(b + n));
 				else
 					printf("  ");
-
+			}
+			printf(" ");
+			for (p = m; p < m + 10; p++)
+			{
+				if (p >= size)
+					break;
+				if (*(b + p) < 32 || *(b + p) > 126)
+					printf("%c", '.');
+				else
+					printf("%c", *(b + p));
+			}
+			printf("\n");
+		}
+	}
+}
