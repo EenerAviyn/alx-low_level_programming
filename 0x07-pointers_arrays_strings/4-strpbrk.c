05b6ea95902fcbf1cd75af5b268605385f212fed
Renée Mulela <42552984+EenerAviyn@users.noreply.github.com>
2022-04-26 23:33:11 +0300

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * _strpbrik - function that locates a substring
- * @s: first string
- * @accept: second string
- * Return: a pointer to byte in s that matches
- * one of the bytes in accept or NULL if none is found
+ * _strpbrk - searches a string for any of a set of bytes.
+ * @s: first string.
+ * @accept: second string.
+ * Return: a pointer to the byte in s that matches one of the
+ * bytes in accept, or NULL if no such byte is found.
  */
-
 char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i, j;
@@ -22,4 +21,3 @@ char *_strpbrk(char *s, char *accept)
 	}
 	return ('\0');
 }
-
