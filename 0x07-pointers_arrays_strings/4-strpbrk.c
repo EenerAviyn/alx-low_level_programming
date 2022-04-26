8a456db2cfd98a350722c3bd4cbb26335d25a33d
EenerAviyn <mulelarenee18@gmail.com>
2022-04-26 13:27:31 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strstr - function that locates a substring
+ * @haystack: string
+ * @needle: substring
+ * Return: a pointer to byte in s that matches
+ * one of the bytes in accept or NULL if none is found
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+				return (s + i);
+		}
+	}
+	return ('\0');
+}
+
