7eb12912f911e9e7488b2b3b8b85ecd78817dc11
Renée Mulela <42552984+EenerAviyn@users.noreply.github.com>
2022-05-03 23:09:17 +0300

    3-islower.c

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -3,9 +3,8 @@
 /**
  * _islower - prints 1 or 0 depending on input
  * @c: first parameter
- *
  * Description: prints all lowercase letters
- * Return: Always(0).
+ * Return: Always(0)
  */
 
 int _islower(int c)
