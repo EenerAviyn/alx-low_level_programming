e5ca72a257fc0a6592d0a3539f2255a017102e10
Renée Mulela <42552984+EenerAviyn@users.noreply.github.com>
2022-05-03 23:08:02 +0300

    2-strncpy.c

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,10 +1,11 @@
 #include "main.h"
+
 /**
- * _strncpy - function that copies string
- * @dest: destination.
- * @src: source.
- * @n: amount of bytes from src.
- * Return: pointer to dest.
+ * _strncpy - copies string
+ * @dest: destination
+ * @src: source
+ * @n: amount of bytes from src
+ * Return: pointer to dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
