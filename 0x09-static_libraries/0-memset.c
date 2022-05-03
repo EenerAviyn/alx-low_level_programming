01fec67d00211cb9e231e6695a5187c770e1218d
Renée Mulela <42552984+EenerAviyn@users.noreply.github.com>
2022-05-03 23:02:20 +0300

    0-memset.c

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * *_memset - function that sets first count bytes to dest 
- * to value c
+ * _memset - fills memory with a constant byte
  * @s: memory area
  * @b: constant byte
  * @n: bytes filled
@@ -14,8 +13,7 @@ char *_memset(char *s, char b, unsigned int n)
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-		*(s + i) = b;
+		*(s + i) =  b;
 
 	return (s);
 }
-
