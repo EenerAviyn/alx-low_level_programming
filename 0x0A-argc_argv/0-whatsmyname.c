0af659861dee40a932cd2b559b53797f93c60525
Renée Mulela <42552984+EenerAviyn@users.noreply.github.com>
2022-05-03 21:09:34 +0300

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,16 +3,15 @@
 
 /**
  * main - Entry point
- * @argc: counts number of paramaters that go into main
- * @argv: pointer of array of pointers containing strings 
- * entering main
+ *
+ * @argc: Counts number of parameters that go into main
+ * @argv: Pointer of array of pointers containing strings entering main
  * Return: Always 0 (Success)
  */
 
 int main(int argc, char **argv)
 {
-	if (argc > 0)
-		printf("%s\n", argv[0]);
-	return (0);
+if (argc > 0)
+printf("%s\n", argv[0]);
+return (0);
 }
-
