9df5553233a416e8eaac25adee39757189022931
EenerAviyn <mulelarenee18@gmail.com>
2022-05-03 08:22:59 -0700

    0-isupper.c

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _is_upper - checks for uppercase characters
+ * @c: character to be checked
+ * Return: 1 if c is uppercase, 0 if lowercase
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
+
