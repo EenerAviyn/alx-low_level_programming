2343539fd4fcd2a91feca8cbc86e01ac56965f01
Renée Mulela <42552984+EenerAviyn@users.noreply.github.com>
2022-05-03 19:57:42 +0300

    0-isupper.c

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _is_upper - checks for uppercase characters
+ * _isupper - function that checks for uppercase characters
  * @c: character to be checked
  * Return: 1 if c is uppercase, 0 if lowercase
  */
