49940599f2ae791be0c695a83b41bade80da0105
Renée Mulela <42552984+EenerAviyn@users.noreply.github.com>
2022-05-05 20:27:30 +0300

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 
 /**
- * count_word - helper function to count number of words 
- * in a string
+ * count_word - helper function to count number of words
  * @s: string to evaluate
  * Return: number of words
  */
