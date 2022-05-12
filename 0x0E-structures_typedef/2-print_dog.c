11d2b37e494defe0c18c2182d35245da67f9e173
Renée Mulela <42552984+EenerAviyn@users.noreply.github.com>
2022-05-12 22:33:24 +0300

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * print_dog - prints a struct dog
@@ -23,4 +24,3 @@ void print_dog(struct dog *d)
 			printf("Owner: %s\n", d->owner);
 	}
 }
-
