0ed0f67c34fcd0d661b75bf357c0704f9d48124d
Renée Mulela <42552984+EenerAviyn@users.noreply.github.com>
2022-05-16 23:35:23 +0300

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,71 +1,60 @@
 #include "3-calc.h"
 
 /**
- * op_add - adds two numbers
- * @a: first number
- * @b: second number
+ * op_add - calculates sum of two integers
+ * @a: first integer
+ * @b: second integer
  * Return: sum
  */
- 
+
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
- * op_sub - subtracts two numbers
- * @a: first number
- * @b: second number
- * Return: difference
+ * op_sub - calculates the difference of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: difference of a and b
  */
- 
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
 /**
- * op_mul - multiplies two numbers
- * @a: first number
- * @b: second number
+ * op_mul - calculates product of two integers
+ * @a: first integer
+ * @b: second integer
  * Return: product
  */
- 
+
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
 /**
- * op_div - divides two numbers
- * @a: first number
- * @b: second number
+ * op_div - calculates division of two integers
+ * @a: first integer
+ * @b: second integer
  * Return: division
  */
- 
+
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a / b);
 }
 
 /**
- * op_mod - calculates the modulo of two numbers
- * @a: first number
- * @b: second number
- * Return: remainder of division
+ * op_mod - calculates remainder of the division of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: remainder
  */
- 
+
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a % b);
 }
