fb6d853f6e4b66121c7925f039254f468ef5b750
Renée Mulela <42552984+EenerAviyn@users.noreply.github.com>
2022-05-16 23:48:36 +0300

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -3,11 +3,11 @@
 
 /**
  * main - prints its own opcodes
- * @argc: count of arguments
+ * @argc: number of arguments
  * @argv: array of arguments
+ *
  * Return: Always 0 (Success)
  */
-
 int main(int argc, char *argv[])
 {
 	int bytes, i;
