39e41942b6907d36d2b2747e192299cd50e9be89
EenerAviyn <mulelarenee18@gmail.com>
2022-05-18 02:28:50 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,7 +1,6 @@
 #include "3-calc.h"
-
 /**
- * main - program that perfroms simple operations
+ * main- program that perfroms simple operations
  * @argc: number of arguments
  * @argv: array of arguments
  * Return: Always 0 (Success)
