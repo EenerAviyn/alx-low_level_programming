4275fdaa8f9923c5e86cc48ec3946684bd25f39b
EenerAviyn <mulelarenee18@gmail.com>
2022-06-02 06:08:06 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -18,11 +18,11 @@ void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
 
 /**
- * check_elf - Checks if a file is an ELF file.
- * @e_ident: A pointer to an array containing the ELF magic numbers.
- *
- * Description: If the file is not an ELF file - exit code 98.
+ * check_elf - function that checks if file is ELF file
+ * @e_ident: pointer to array containing ELF magic numbers
+ * Description: 98 exit code
  */
+
 void check_elf(unsigned char *e_ident)
 {
 int index;
