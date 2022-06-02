f7080c7c0e864ff399e03dcfb0d7aed80402f0a9
Renée Mulela <42552984+EenerAviyn@users.noreply.github.com>
2022-06-02 22:50:31 +0300

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - Reads text file and prints to POSIX stdout
- * @filename: pointer to name of file
- * @letters: number of letters function should read and print
- * Return: If the function fails or 0
+ * read_textfile - Reads a text file and prints it to POSIX stdout
+ * @filename: A pointer to the name of the file
+ * @letters: The number of letters function should read and print
+ * Return: If the function fails or filename is NULL - 0.
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
@@ -15,9 +15,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
+
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
+
 	o = open(filename, O_RDONLY);
 	r = read(o, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, r);
@@ -29,5 +31,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 	free(buffer);
 	close(o);
+
 	return (w);
 }
