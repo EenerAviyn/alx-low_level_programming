8f65c37ee432ab937c36d9a11c26e86fcd964f0f
Renée Mulela <42552984+EenerAviyn@users.noreply.github.com>
2022-07-15 23:50:15 +0300

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/arthurdamm/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libwinner.so
+wget -P /tmp https://github.com/baccrie/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
+export LD_PRELOAD=/tmp/libmask.so
